c6d4621c01606627ca65897915f41b0eb9ba9d3f
soyeon\yeon <isylsy166@gmail.com>
2024-05-08 13:57:49 +0900

    조회 기능

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,10 +5,12 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 @Slf4j
 public class MemberRepositoryV0 {
 
+    /*등록*/
     public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id,money) values (?, ?)";
 
@@ -31,7 +33,41 @@ public class MemberRepositoryV0 {
         finally {
             close(con, pstmt, null);
         }
+    }
 
+    /*조회*/
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1,memberId);
+            
+            rs = pstmt.executeQuery(); //쿼리 결과
+
+            if(rs.next()){
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }
+            else{
+                //데이터가 없을 때
+                throw new NoSuchElementException("member not found memberId" + memberId);
+            }
+        }
+        catch (SQLException e) {
+            log.error("DB error", e);
+            throw e;
+        }
+        finally {
+            close(con, pstmt, rs);
+        }
 
     }
 
